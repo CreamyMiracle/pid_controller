dd45b65152bfc8d9e431b12500494263d5b6f527
Juhana Rajala <juhana.rajala@vertex.fi>
2022-05-30 22:21:46 +0300

    Initial commit

---
+++ PIDController/PIDController/PIDController.cs
@@ -0,0 +1,158 @@
+﻿namespace PIDController
+{
+    public class PIDController
+    {
+        private float proportionalGain;
+        private float integralGain;
+        private float derivativeGain;
+
+        private float lastError;
+        private float lastValue;
+
+        private float integrationStored;
+        private float integralSaturation;
+
+        private float outMin;
+        private float outMax;
+
+        private DerivativeMeasurement derivativeMeasurement = DerivativeMeasurement.Smth;
+        private SystemType systemType = SystemType.Linear;
+
+        private bool derivativeInitialized = false;
+
+        public PIDController(float _proportionalGain, float _integralGain, float _derivativeGain, float _outMin, float _outMax, DerivativeMeasurement _derivativeMeasurement, SystemType _systemType, float _integralSaturation)
+        {
+            proportionalGain = _proportionalGain;
+            integralGain = _integralGain;
+            derivativeGain = _derivativeGain;
+
+            outMin = _outMin;
+            outMax = _outMax;
+
+            derivativeMeasurement = _derivativeMeasurement;
+
+            systemType = _systemType;
+            integralSaturation = _integralSaturation;
+        }
+
+        public float Update(float dTime, float currValue, float targetValue)
+        {
+            float currError = Error(currValue, targetValue);
+
+            float dValue = ValueDiff(currValue, dTime);
+            float dError = ErrorDiff(currError, dTime);
+
+            // Proportional to error
+            float p_term = P(currError);
+
+            // Continuos error elimination
+            float i_term = I(currError, dTime);
+
+            // Fights the rate of change of the error (or value)
+            float d_term = D(dError, dValue);
+
+            float result = p_term + d_term + i_term;
+
+            return Math.Clamp(result, outMin, outMax);
+        }
+
+        public void Reset()
+        {
+            derivativeInitialized = false;
+        }
+
+        private float ValueDiff(float currValue, float dTime)
+        {
+            float dValue;
+            if (systemType.Equals(SystemType.Angular))
+            {
+                dValue = AngularDifference(currValue, lastValue) / dTime;
+            }
+            else
+            {
+                dValue = LinearDifference(lastValue, currValue) / dTime;
+            }
+            lastValue = currValue;
+            return dValue;
+        }
+
+        private float ErrorDiff(float currError, float dTime)
+        {
+            float dError;
+            if (systemType.Equals(SystemType.Angular))
+            {
+                dError = AngularDifference(currError, lastError) / dTime;
+            }
+            else
+            {
+                dError = LinearDifference(lastError, currError) / dTime;
+            }
+            lastError = currError;
+            return dError;
+        }
+
+        private float Error(float currValue, float targetValue)
+        {
+            if (systemType.Equals(SystemType.Angular))
+            {
+                return AngularDifference(currValue, targetValue);
+            }
+            else
+            {
+                return LinearDifference(currValue, targetValue);
+            }
+        }
+
+        private float P(float error)
+        {
+            return proportionalGain * error;
+        }
+
+        private float I(float error, float dTime)
+        {
+            integrationStored = integrationStored + (error * dTime);
+            return Math.Clamp(integralGain * integrationStored, -integralSaturation, integralSaturation);
+        }
+
+        private float D(float dError, float dValue)
+        {
+            if (!derivativeInitialized)
+            {
+                return 0;
+            }
+            derivativeInitialized = true;
+
+            // To prevent derivative kicks
+            if (derivativeMeasurement.Equals(DerivativeMeasurement.Velocity))
+            {
+                return derivativeGain * -dValue;
+            }
+            else
+            {
+                return derivativeGain * dError;
+            }
+        }
+
+        private float LinearDifference(float currValue, float targetValue)
+        {
+            return targetValue - currValue;
+        }
+        
+        private float AngularDifference(float currValue, float targetValue)
+        {
+            return (currValue - targetValue + 540) % 360 - 180;
+        }
+    }
+
+    public enum DerivativeMeasurement
+    {
+        Velocity,
+        Smth
+    }
+
+    public enum SystemType
+    {
+        Linear,
+        Angular
+    }
+}
